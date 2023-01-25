dbfe4f7596faf5f7f370ae7adb37460b68ff74b4
topscoder <86197446+topscoder@users.noreply.github.com>
2023-01-25 15:30:21 +0100

    Panic at the disco 🪩

--- lurk.py
+++ lurk.py
@@ -2,6 +2,7 @@ import json
 import os
 import requests
 
+
 # Reading settings
 with open(".settings.json") as settings:
     data = json.load(settings)
@@ -41,27 +42,38 @@ def update_progress(project="", file=""):
 
 
 def get_progress():
-    progress = ""
-    with open("progress.rc", "w+") as f:
-        progress = f.read()
+    progress = json.loads('{ "project": "", "file": "" }')
+    with open("progress.rc", "r") as f:
+        content = f.read()
+        if content != "":
+            progress = json.loads(content)
+
     return progress
 
 
-# Create with fresh progress file
-update_progress("")
+progress = get_progress()
+if progress["project"] != "":
+    resume = True
+else:
+    # Create with fresh progress file
+    resume = False
+    update_progress("")
+
+# resume = False
 
 # First get all projects
 print("# Fetching all projects ...")
 r = requests.get(f"{base_url}/api/projects/search", auth=(username, password))
 js = r.json()
 
-print("# Dump to projects.json ...")
-write_file_in_dir("lurked/projects.json", json.dumps(js, indent=4))
+if resume is False:
+    print("# Dump to projects.json ...")
+    write_file_in_dir("lurked/projects.json", json.dumps(js, indent=4))
 
-update_progress("lurked/projects.json")
+    update_progress("lurked/projects.json")
 
 # For each project, get all filenames
-print("# Fetch files for each project ...")
+print("# Download files for each project ...")
 for key, value in js.items():
     if key != "components":
         continue
@@ -69,21 +81,69 @@ for key, value in js.items():
     for row in value:
         project_key = row.get("key")
         print("")
-        print(f"## Project {project_key}...")
+
+        if resume is True:
+            if project_key == progress["project"]:
+                print(f"[{project_key}] Resuming this project...")
+                resume = False
+            else:
+                print(f"[{project_key}] Skipping {project_key}...")
+                continue
+
+        if resume is True and progress["file"] == "":
+            resume = False
+
+        print(f"## [{project_key}] Project {project_key}...")
 
         rp = requests.get(
             f"{base_url}/batch/project?key={project_key}", auth=(username, password))
 
         # write sources.json
-        print(f"## Write lurked/{project_key}.json ...")
-        sources = rp.json()
-        write_file_in_dir(f"lurked/{project_key}.json", sources)
+        try:
+            sources = rp.json()
+        except Exception as e:
+            print(f"### {project_key} ERROR Failed to parse project file.")
+            print(f"Exception: {e}")
+            print(rp.text)
+            write_file_in_dir(f"lurked/{project_key}.failed", rp.text)
+            continue
 
-        update_progress(project_key, f"lurked/{project_key}.json")
+        if resume is False:
+            print(f"## [{project_key}] Write lurked/{project_key}.json ...")
+            write_file_in_dir(f"lurked/{project_key}.json", sources)
+            update_progress(project_key, f"lurked/{project_key}.json")
 
         # get source code for each project file
+        if "settingsByModule" in sources \
+            and project_key in sources["settingsByModule"] \
+                and "sonar.scm.provider" in sources["settingsByModule"][project_key]:
+            print(f"## [{project_key}] Write lurked/{project_key}/scm.json ...")
+            write_file_in_dir(f"lurked/{project_key}/scm.json", sources)
+            continue
+
+        if not sources["fileDataByModuleAndPath"] or not sources["fileDataByModuleAndPath"][project_key]:
+            print(f"## [{project_key}] ERROR Unknown source structure ...")
+            write_file_in_dir(f"lurked/{project_key}/source.json", sources)
+            continue
+
         file_list = sources["fileDataByModuleAndPath"][project_key]
         for filename in file_list:
+            if resume is True:
+                if filename == progress["file"]:
+                    print(f" [{project_key}]Resuming {filename}")
+                    resume = False
+                else:
+                    print(f" [{project_key}]Skipping {filename}...")
+                    continue
+
+            # at this point, resume should always be False
+            # if not, we should resume but couldnt find the right file.
+            # so better is to start fresh.
+            if resume is True:
+                print(f"### [{project_key}] ERROR: We could not find the right file to resume from.")
+                print("Please remove progress.rc and restart this script.")
+                exit(1)
+
             # fetch source code and store in local file
             update_progress(project_key, filename)
 
@@ -92,18 +152,21 @@ for key, value in js.items():
                 auth=(username, password))
 
             if rs.status_code != 200:
-                print(f"--- PANIC AT THE DISCO 🪩")
-                print(f"--- HTTP {rs.status_code}")
-                print(f"--- Error fetching {base_url}/api/sources/index?key={project_key} & resource={project_key}: {filename}")
-                print(f"--- Skipping this one, but let's dance again..")
+                print(f"[{project_key}] --- PANIC AT THE DISCO 🪩")
+                print(f"[{project_key}] --- HTTP {rs.status_code}")
+                print(
+                    f"[{project_key}] --- Error fetching {base_url}/api/sources/index?key={project_key} & resource={project_key}: {filename}")
+                print(
+                    f"[{project_key}] --- Skipping this one, but let's dance again..")
                 continue
 
             if rs is None:
-                print(f"--- PANIC AT THE DISCO 🪩")
-                print(f"--- HTTP None")
+                print(f"[{project_key}] --- PANIC AT THE DISCO 🪩")
+                print(f"[{project_key}] --- HTTP None")
                 print(
-                    f"--- Error fetching {base_url}/api/sources/index?key={project_key} & resource={project_key}: {filename}")
-                print(f"--- Skipping this one, but let's dance again..")
+                    f"[{project_key}] --- Error fetching {base_url}/api/sources/index?key={project_key} & resource={project_key}: {filename}")
+                print(
+                    f"[{project_key}] --- Skipping this one, but let's dance again..")
                 continue
 
             file_lines = rs.json()
@@ -112,7 +175,7 @@ for key, value in js.items():
                 for line in key:
                     file_str += key[line] + "\n"
 
-            print(f"### Writing {filename}")
+            print(f"### [{project_key}] Writing {filename}")
 
             write_file_in_dir(
                 os.path.join("lurked/", project_key, filename), file_str, is_json=False)
